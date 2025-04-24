30fd23b620150750232629eb0757f63c553b7ec9
Robb Walters <robb@2amlogic.com>
2025-04-23 17:47:44 -0700

    fix: just use find_packages

--- setup.py
+++ setup.py
@@ -303,8 +303,7 @@ setup(
     "PufferAI's library of RL tools and utilities",
     long_description_content_type="text/markdown",
     version=VERSION,
-    packages=find_packages(where="pufferlib"),
-    package_dir={"": "pufferlib"},
+    packages=find_packages(),
     package_data={
         "pufferlib": [
             f'{RAYLIB_LIB}/libraylib.a',
