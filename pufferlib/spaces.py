0a6e60abeb45bd6629a221b4ce5364bd32937cf7
David Bloomin <daveey@gmail.com>
2025-05-05 00:55:58 -0700

    use gymnasium in spaces, and update to numpy2

--- pufferlib/spaces.py
+++ pufferlib/spaces.py
@@ -1,5 +1,5 @@
 import numpy as np
-import gym
+import gymnasium as gym
 import gymnasium
 
 Box = (gym.spaces.Box, gymnasium.spaces.Box)
@@ -9,17 +9,20 @@ MultiBinary = (gym.spaces.MultiBinary, gymnasium.spaces.MultiBinary)
 MultiDiscrete = (gym.spaces.MultiDiscrete, gymnasium.spaces.MultiDiscrete)
 Tuple = (gym.spaces.Tuple, gymnasium.spaces.Tuple)
 
+
 def joint_space(space, n):
     if isinstance(space, Discrete):
         return gymnasium.spaces.MultiDiscrete([space.n] * n)
     elif isinstance(space, MultiDiscrete):
-        return gymnasium.spaces.Box(low=0,
-            high=np.repeat(space.nvec[None] - 1, n, axis=0),
-            shape=(n, len(space)), dtype=space.dtype)
+        return gymnasium.spaces.Box(
+            low=0, high=np.repeat(space.nvec[None] - 1, n, axis=0), shape=(n, len(space)), dtype=space.dtype
+        )
     elif isinstance(space, Box):
         return gymnasium.spaces.Box(
             low=np.repeat(space.low[None], n, axis=0),
             high=np.repeat(space.high[None], n, axis=0),
-            shape=(n, *space.shape), dtype=space.dtype)
+            shape=(n, *space.shape),
+            dtype=space.dtype,
+        )
     else:
-        raise ValueError(f'Unsupported space: {space}')
+        raise ValueError(f"Unsupported space: {space}")
