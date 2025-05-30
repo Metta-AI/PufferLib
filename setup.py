89bfee8e048a283e817b854c8520bd098fcf3c43
Richard Higgins <richard@softmax.com>
2025-05-29 17:14:47 -0700

    back to numpy <2

--- setup.py
+++ setup.py
@@ -465,7 +465,7 @@ setup(
     },
     include_package_data=True,
     install_requires=[
-        'numpy==2.2.6',
+        'numpy<2',
         'opencv-python==3.4.17.63',
         'rich',
         'rich_argparse',
